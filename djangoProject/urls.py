543b353dc152edb022ec73422e612ed2f8744741
melvinportillo <eliudportillo26@hotmail.com>
2021-10-19 22:56:50 -0600

    Config url

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core.views import HomeView
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
